ddab9fb3bb067692cbccd07cef5ef6c08e6eb09d
Yosuke Yanai <y1y1ytti@gmail.com>
2016-11-29 07:47:38 +0000

    add have and want button

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,7 +11,7 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      items = RakutenWebService::Ichiba::Item.search( itemCode: params[:item_code])
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -25,8 +25,12 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
-
+    type = params[:type]
+    if type == "Have"
+      current_user.have(@item) unless current_user.have?(@item)
+    elsif type == "Want"
+      current_user.want(@item) unless current_user.want?(@item)
+    end
   end
 
   def destroy
@@ -35,6 +39,10 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
-
+    if type == Have
+      current_user.unlike(@item) if current_user.have?(@item)
+    elsif type == Want
+      current_user.unwant(@item) if current_user.want?(@item)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,13 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
-
+  
+  has_many :wants, class_name: Want, foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  
+  has_many :haves, class_name: Have, foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
+ 
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.create(followed_id: other_user.id)
@@ -31,20 +36,28 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
+    have = haves.find_by(item_id: item.id)
+    have.destroy if have
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    want = wants.find_by(item_id: item.id)
+    want.destroy if want
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
